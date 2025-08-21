477df90483be61ee8d67e4f85b836f2ab1cf73ff
Jorge Eduardo Herrera Garcia <z-5green-360@hotmail.com>
2025-08-21 11:43:49 -0600

    Initial commit

---
+++ metasApp/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hola, como te llamas?</p>
+    </div>
+  )
+}
+
+export default App
